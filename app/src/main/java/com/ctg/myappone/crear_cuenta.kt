20e9d80bcb4c4bb53a6adef17e28e1ef2ea7d4b7
JHONDRASCODE <132631552+JHONDRASCODE@users.noreply.github.com>
2024-07-26 22:01:23 -0500

    Se Agrego Primero

---
+++ app/src/main/java/com/ctg/myappone/crear_cuenta.kt
@@ -0,0 +1,20 @@
+package com.ctg.myappone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class crear_cuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_crear_cuenta)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
